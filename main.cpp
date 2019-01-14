756ff478e5389cdcd16632bee034b9f21ff99d66
dextricyril <truitecendree@gmail.com>
2019-01-14 21:23:45 +0100

    MOD local main and compile files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "CGameSettings.h"
+#include "locale.h"
 
 int main(int argc, char ** argv)
 {
+    std::setlocale(LC_CTYPE, "fr_FR.UTF-8");
     if(argc != 3)
     {
         std::cerr << "Not the right amount of file provided" << std::endl; 
@@ -12,6 +14,6 @@ int main(int argc, char ** argv)
     std::string fileWeapon = argv[2];
     std::cout << "  Fichier 1 :   " << fileCharacter << "   Fichier 2 :   " << fileWeapon << std::endl;;
 
-    CGameSettings(fileCharacter, fileWeapon);
-    
+    CGameSettings* setting = new  CGameSettings(fileCharacter, fileWeapon);
+    setting->printCharacterList();
 }
